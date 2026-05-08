a200cd03abdbffbe992ce4eeac56eef994dc3243
Riwal Clech <84081096+Polaris6916@users.noreply.github.com>
2026-05-08 18:20:42 +0200

    Update Visual Studio version in build script

--- build_vs.bat
+++ build_vs.bat
@@ -2,4 +2,4 @@ rem Use this batch file to build for Visual Studio 17
 rmdir /s /q _build_vs
 mkdir _build_vs
 cd _build_vs
-cmake .. -G "Visual Studio 17 2022" -A x64
+cmake .. -G "Visual Studio 18 2026" -A x64
